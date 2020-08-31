109b4bcf70a220144843f684f1a05968d7d3f7ba
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2020-08-31 09:11:33 -0400

    added a software notes file

---
+++ softwareNotes.R
@@ -0,0 +1,14 @@
+
+
+# quick note--what do users need to do software/package wise, and where do we have the instructions?
+
+### Get SyncroSim
+# On STSM page.  Looks good.
+
+### ST-Sim Package
+# Two options look good-could add more instructions for Option 2--what do you do once you have downloaded?
+
+### LANDFIRE Vegmodels
+# Add to STSM page?
+
+
